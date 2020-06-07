9777ac99ace3c3e6f380b15da67a044b32071101
nicolae_taban <nicolae.taban@gmail.com>
2020-06-07 15:18:00 +0300

    Adds a drawable picturebox; Starts significant point detection;

--- BinarySynapticWeightsPointsApplication/UI.Designer.cs
+++ BinarySynapticWeightsPointsApplication/UI.Designer.cs
@@ -28,18 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.drawingPanel = new System.Windows.Forms.Panel();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.drawingPictureBox = new System.Windows.Forms.PictureBox();
+            this.SignificantPointsButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.drawingPictureBox)).BeginInit();
             this.SuspendLayout();
-            // 
-            // drawingPanel
-            // 
-            this.drawingPanel.BackColor = System.Drawing.Color.White;
-            this.drawingPanel.Location = new System.Drawing.Point(12, 12);
-            this.drawingPanel.Name = "drawingPanel";
-            this.drawingPanel.Size = new System.Drawing.Size(384, 384);
-            this.drawingPanel.TabIndex = 0;
             // 
             // button1
             // 
@@ -61,25 +55,49 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // drawingPictureBox
+            // 
+            this.drawingPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.drawingPictureBox.Location = new System.Drawing.Point(12, 12);
+            this.drawingPictureBox.Name = "drawingPictureBox";
+            this.drawingPictureBox.Size = new System.Drawing.Size(384, 387);
+            this.drawingPictureBox.TabIndex = 3;
+            this.drawingPictureBox.TabStop = false;
+            this.drawingPictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.drawingPictureBox_MouseDown);
+            this.drawingPictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.drawingPictureBox_MouseMove);
+            this.drawingPictureBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.drawingPictureBox_MouseUp);
+            // 
+            // SignificantPointsButton
+            // 
+            this.SignificantPointsButton.Location = new System.Drawing.Point(402, 119);
+            this.SignificantPointsButton.Name = "SignificantPointsButton";
+            this.SignificantPointsButton.Size = new System.Drawing.Size(88, 37);
+            this.SignificantPointsButton.TabIndex = 4;
+            this.SignificantPointsButton.Text = "Get Significant Points";
+            this.SignificantPointsButton.UseVisualStyleBackColor = true;
+            this.SignificantPointsButton.Click += new System.EventHandler(this.SignificantPointsButton_Click);
+            // 
             // UI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(500, 411);
+            this.Controls.Add(this.SignificantPointsButton);
+            this.Controls.Add(this.drawingPictureBox);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.drawingPanel);
             this.Name = "UI";
-            this.Text = "Form1";
+            this.Text = "Online Shape Recognition";
+            ((System.ComponentModel.ISupportInitialize)(this.drawingPictureBox)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel drawingPanel;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox drawingPictureBox;
+        private System.Windows.Forms.Button SignificantPointsButton;
     }
 }
 
--- BinarySynapticWeightsPointsApplication/UI.cs
+++ BinarySynapticWeightsPointsApplication/UI.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
 using System.Windows.Forms;
 using BinarySynapticWeights;
 
@@ -7,6 +9,13 @@ namespace BinarySynapticWeightsPointsApplication
 {
     public partial class UI : Form
     {
+        private Graphics graphics;
+        private Pen regularPen;
+        private Brush significantPoinBrush;
+        private List<Stroke> strokes = new List<Stroke>();
+        private Stroke currentStroke;
+        private Point lastDrawnPoint;
+        // start of legacy members
         private int[,] matrix;
         private readonly int matrixSize = 64;
         private readonly int scale = 6;
@@ -19,22 +28,27 @@ namespace BinarySynapticWeightsPointsApplication
 
         private NeuralNetworkTrainingAlgorithm binarySynapticWeights;
         private BSWModel bswModel;
+        // end of legacy members
 
         public UI()
         {
             InitializeComponent();
 
             matrix = TrainingSets.GetThirdTrainingSet(matrixSize);
+            graphics = drawingPictureBox.CreateGraphics();
+            regularPen = new Pen(Color.Black, 2);
+            significantPoinBrush = (Brush)Brushes.Green;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            DrawTrainingPoints();
+            //DrawTrainingPoints();
         }
 
+        /*
         private void DrawTrainingPoints()
         {
-            var graphics = drawingPanel.CreateGraphics();
+            var graphics = drawing.CreateGraphics();
 
             for (int i = 0; i < matrixSize; i++)
             {
@@ -48,9 +62,12 @@ namespace BinarySynapticWeightsPointsApplication
                 }
             }
         }
+        */
+
 
         private void button2_Click(object sender, EventArgs e)
         {
+            /*
             binarySynapticWeights = new NeuralNetworkTrainingAlgorithm();
 
             var trainingSamples = Preprocessing.GetTrainingSamplesFromMatrix(matrix);
@@ -58,11 +75,62 @@ namespace BinarySynapticWeightsPointsApplication
             bswModel = binarySynapticWeights.Train(trainingSamples);
 
             ClassifyAllPixels();
+            */
         }
 
+        private void drawingPictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                currentStroke = new Stroke(e.Location);
+            }
+        }
+
+        private void drawingPictureBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Point currentPoint = e.Location;
+                graphics.DrawLine(regularPen, lastDrawnPoint, currentPoint);
+                lastDrawnPoint = currentPoint;
+                currentStroke.AppendPoint(currentPoint);
+            }
+        }
+
+        private void drawingPictureBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (currentStroke.GetNumberOfPoints() > 1)
+            {
+                strokes.Add(currentStroke);
+            }
+        }
+
+        private void SignificantPointsButton_Click(object sender, EventArgs e)
+        {
+            List<Point> significantPoints = new List<Point>();
+            foreach (Stroke s in strokes)
+            {
+                List<Point> significantPointsOnCurrentStroke = s.GetSignificantPoints();
+                DrawSignificantPoints(significantPointsOnCurrentStroke);
+            }
+
+        }
+
+        private void DrawSignificantPoints(List<Point> significantPointsOnCurrentStroke)
+        {
+            foreach (Point p in significantPointsOnCurrentStroke)
+            {
+                graphics.FillRectangle(significantPoinBrush, p.X - 4, p.Y - 4, 9, 9);
+            }
+        }
+
+
+
+
+        /*
         private void ClassifyAllPixels()
         {
-            var graphics = drawingPanel.CreateGraphics();
+            var graphics = drawing.CreateGraphics();
 
             for (int i = 0; i < 64; i++)
             {
@@ -84,13 +152,16 @@ namespace BinarySynapticWeightsPointsApplication
                 }
             }
         }
+        */
 
-        private void DrawPoint(Graphics graphics, Pen pen, int x, int y)
-        {
-            graphics.DrawRectangle(pen, x * scale, y * scale, 4, 4);
-            graphics.DrawRectangle(pen, x * scale, y * scale, 3, 3);
-            graphics.DrawRectangle(pen, x * scale, y * scale, 2, 2);
-            graphics.DrawRectangle(pen, x * scale, y * scale, 1, 1);
-        }
+        /*
+    private void DrawPoint(Graphics graphics, Pen pen, int x, int y)
+    {
+        graphics.DrawRectangle(pen, x * scale, y * scale, 4, 4);
+        graphics.DrawRectangle(pen, x * scale, y * scale, 3, 3);
+        graphics.DrawRectangle(pen, x * scale, y * scale, 2, 2);
+        graphics.DrawRectangle(pen, x * scale, y * scale, 1, 1);
+    }
+    */
     }
 }
