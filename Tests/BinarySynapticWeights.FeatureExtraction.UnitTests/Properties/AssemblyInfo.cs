af87a9ee3dab592a968b15bc0463aa76aedd51ae
Marius Vladu <marius.vladu96@gmail.com>
2020-06-07 17:36:47 +0300

    Added logic to calculate center of gravity

---
+++ Tests/BinarySynapticWeights.FeatureExtraction.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BinarySynapticWeights.FeatureExtraction.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BinarySynapticWeights.FeatureExtraction.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2501616c-c423-4317-9734-0a784406d940")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
